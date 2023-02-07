5ae40f51eaf2a43c8226bff7f4df3b3c87083839
Gustavo Enricco Goncalves Alexandre <gustavoalexandre@employer.com.br>
2023-02-07 17:12:43 -0300

    Adicionar arquivos de projeto.

---
+++ MVCIdentity/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MVCIdentity.Models;
+
+namespace MVCIdentity.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Contato> Contatos { get; set; }
+    }
+}
---
+++ MVCIdentity/Models/Contato.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCIdentity.Models
+{
+    [Table("Contatos")]
+    public class Contato
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+    }
+}
